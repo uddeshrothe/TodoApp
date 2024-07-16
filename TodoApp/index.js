29f65fb1e612e52a5a908d2df7952b66080a0817
uddeshrothe <uddesh.rothe1@gmail.com>
2024-07-16 16:50:17 +0530

    updated schema and added delete method

--- TodoApp/index.js
+++ TodoApp/index.js
@@ -23,12 +23,41 @@ app.get('/api/tasklist', async (req, res) => {
 });
 
 app.post('/api/addtask', async (req, res) => {
+    
     try {
         const task = await Task.create(req.body)
         res.status(200).json(task)
-    } catch (error) {
+    } catch (err) {
         res.status(500).json({ message: err.message });
     }
 })
+
+app.put('/api/updatetask/:id', async (req, res) => {
+    
+    try {
+        const {id} = req.params
+        const task = await Task.findByIdAndUpdate(id, req.body)
+        if (!task) {
+            res.status(404).json({message: "Task not found"})
+        }
+        res.status(200).json({ message: "Task updated successfully" })
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
+app.delete('/api/deletetask/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const task = await Task.findByIdAndDelete(id)
+        if (!task) {
+            res.status(404).json({message: "Task not found"})
+        }
+        res.status(200).json({ message: "Task completed successfully" })
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
   
 app.listen(3000, ()=>{ console.log("Server is running!");})
