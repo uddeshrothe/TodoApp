09bc46be51f2656f31eb7d185ba2400904f6c0c1
uddeshrothe <uddesh.rothe1@gmail.com>
2024-07-16 15:03:11 +0530

    added incremental id counter

--- TodoApp/models/taskmodel.js
+++ TodoApp/models/taskmodel.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose')
 
 // Task Schema
 const taskSchema = new mongoose.Schema({
-    id: { type: String, required: true },
+    id: { type: Number, unique:true },
     description: { type: String, required: true },
 });
+
+taskSchema.statics.getNextTaskId = async function() {
+    const lastTask = await this.findOne().sort({ id: -1 });
+    return lastTask ? lastTask.id + 1 : 1;
+  };
+  
+  taskSchema.pre('save', async function (next) {
+    if (this.isNew) {
+      this.id = await mongoose.model('Task').getNextTaskId();
+    }
+    next();
+  });
   
 const Task = mongoose.model("Task", taskSchema)
 
